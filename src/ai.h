6628c7317801dbaca17b6d50ac137be992494d50
Besnik Picori <picori.besnik@gmail.com>
2026-04-25 23:10:06 +0200

    chore: cleanups

--- src/ai.h
+++
@@ -1,8 +0,0 @@
-#ifndef AI_H
-#define AI_H
-
-#include "game.h"
-
-Direction choose_direction_toward_food(const Game* game);
-
-#endif
--- src/main.c
+++ src/main.c
@@ -7,9 +7,10 @@
 #include "SDL_video.h"
 #include "brain.h"
 #include "config.h"
-#include "render.h"
+#include "game.h"
 #include "training.h"
 
+#define CELL_SIZE 30
 #define WINDOW_WIDTH (GRID_WIDTH * CELL_SIZE)
 #define WINDOW_HEIGHT (GRID_HEIGHT * CELL_SIZE)
 
@@ -67,6 +68,33 @@ static void handle_input(bool* running) {
       }
     }
   }
+}
+
+static void render_cell(SDL_Renderer* renderer, Vec2 position) {
+  SDL_Rect rect = {position.x * CELL_SIZE, position.y * CELL_SIZE, CELL_SIZE - 1, CELL_SIZE - 1};
+  SDL_RenderFillRect(renderer, &rect);
+}
+
+static void render_game(SDL_Renderer* renderer, const Game* game) {
+  SDL_SetRenderDrawColor(renderer, 20, 20, 20, 255);
+  SDL_RenderClear(renderer);
+
+  SDL_SetRenderDrawColor(renderer, 220, 40, 40, 255);
+  render_cell(renderer, game->food);
+
+  for (int i = game->snake.length - 1; i >= 0; i--) {
+    if (i == 0) {
+      SDL_SetRenderDrawColor(renderer, 255, 240, 80, 255);
+    } else if (i == game->snake.length - 1) {
+      SDL_SetRenderDrawColor(renderer, 0, 130, 70, 255);
+    } else {
+      SDL_SetRenderDrawColor(renderer, 0, 220, 80, 255);
+    }
+
+    render_cell(renderer, game->snake.body[i]);
+  }
+
+  SDL_RenderPresent(renderer);
 }
 
 static bool update_simulation(Game* game, TrainingSession* session) {
